e4940dd7bd31f9b512ebc29d33659039e545586c
Patrick Avery <psavery@buffalo.edu>
2017-07-25 14:29:01 -0400

    Starts the FCM

---
+++ friendly_computing_machine/__init__.py
@@ -0,0 +1,6 @@
+"""
+This is the base file of the friendly-computing-machine
+"""
+
+from . import math
+from .math import mult
---
+++ friendly_computing_machine/math.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""A small set of functions for doing math operations.
+"""
+
+# Write a function named add that adds two values
+def add(a, b):
+    """Function that adds two arguments
+    """
+    return a + b
+
+def sub(a, b):
+    """Function that subtracts two arguments
+    """
+    return a - b
+
+def mult(a, b):
+    """Function that multiplies two arguments
+    """
+    return a * b
+
+def div(a, b):
+    """Function that divides two arguments
+    """
+    return a / b
+
+def intDiv(a, b):
+    """Function that divides two arguments with integer division
+    """
+    return a // b
