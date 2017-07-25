46dae22e4b26b5382e600d93f9aaabab4f321a06
Patrick Avery <psavery@buffalo.edu>
2017-07-25 14:47:09 -0400

    Added setup.py and tests

---
+++ tests/test_math.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Simple tests for the math module
+"""
+
+import friendly_computing_machine as fcm
+import pytest
+
+def test_add():
+    assert fcm.math.add(5, 2) == 7
+    assert fcm.math.add(2, 5) == 7
